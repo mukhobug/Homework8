0eb6e45f5f84b45c089d43e543269bb2bae5a150
Alejandro <mukhobug@gmail.com>
2022-09-05 20:01:37 +0300

    homework done

---
+++ src/Library.java
@@ -0,0 +1,43 @@
+public class Library {
+    private final Book[] library;
+
+    public Library(int size) {
+        library = new Book[size];
+    }
+
+    public void addBookToLibrary(Book name) {
+        for (int i = 0; i < library.length; i++) {
+            if (library[i] == null) {
+                library[i] = name;
+                return;
+            }
+        }
+    }
+
+    public void libraryFullBookName() {
+        for (Book book : library) {
+            if (book != null) {
+                System.out.println(book.getAuthor().getFirstName() + " " + book.getAuthor().getLastName() + ": " + book.getName() + ": " + book.getPublishYear());
+            }
+        }
+    }
+
+    public void findName(String name) {
+        for (Book book : library) {
+            if (name.equals(book.getName())) {
+                System.out.printf("%s by %s %s was published in %d", book.getName(), book.getAuthor().getFirstName(), book.getAuthor().getLastName(), book.getPublishYear());
+                return;
+            }
+        }
+    }
+
+    public void findNameAndChangeYear(String name, int year) {
+        for (Book book : library) {
+            if (name.equals(book.getName())) {
+                book.setPublishYear(year);
+                System.out.printf("\nBook year changed (don't try to rewrite history): %d", book.getPublishYear());
+                return;
+            }
+        }
+    }
+}
